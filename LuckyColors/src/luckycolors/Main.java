445de2f9a46ff2286f4b4cbe02eba610d6ad739d
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2024-10-02 09:31:05 -0600

    Add files via upload

---
+++ LuckyColors/src/luckycolors/Main.java
@@ -0,0 +1,20 @@
+package luckycolors;
+
+/*
+Carlos Alberto Cerda Altamirano 
+No.Control 23550348
+
+Daniel Isaac Gandara Tapia 
+No.Control 23550374
+
+Estructura de Datos 
+Grupo C 
+Inciado en 2 Octubre 2024 * 8:48
+ */
+public class Main {
+    public static void main(String[] args) {
+        LuckyPanel window = new LuckyPanel();
+        window.setVisible(true);
+    }
+    
+}
