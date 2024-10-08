cab29fc3aedc40d05352387852a35e0140272d23
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2024-10-08 15:10:34 -0600

    Add files via upload

---
+++ luckycolors/LuckyColors.java
@@ -0,0 +1,18 @@
+package luckycolors;
+
+/*
+Carlos Alberto Cerda Altamirano
+No.C 23550348
+Daniel Gandara Tapia 
+No.C 25503--
+
+Grupo C 
+Estructura de Datos
+ */
+public class LuckyColors {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
