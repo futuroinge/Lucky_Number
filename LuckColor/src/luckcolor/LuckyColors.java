0a27b213cebd394ce033f8210b98afb04763bed1
DanGandx <143562619+DanGandx@users.noreply.github.com>
2024-10-23 20:16:31 -0600

    Add files via upload

---
+++ LuckColor/src/luckcolor/LuckyColors.java
@@ -0,0 +1,17 @@
+package luckcolor;
+
+/*
+Carlos Alberto Cerda Altamirano
+No.C 23550348
+Daniel Gandara Tapia 
+No.C 2550374
+Grupo C 
+Estructura de Datos
+ */
+public class LuckyColors {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
